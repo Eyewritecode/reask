a80ee4c3c46daae49993233c2385407d2cec9c25
eyewritecode <933kss@gmail.com>
2018-03-14 17:05:31 +0900

    added CSS loaders and extract-text webpack plugin

--- static/webpack.config.js
+++ static/webpack.config.js
@@ -1,5 +1,23 @@
 const webpack = require("webpack");
 
+/* Add "extract-text-webpack-plugin" 
+  https://webpack.js.org/plugins/extract-text-webpack-plugin/#components/sidebar/sidebar.jsx
+  
+  To be able to use CSS in our application, we have to install a few loaders and plugins, and add them to our Webpack config. This is because Webpack only understands JavaScript.
+
+  Install the following plugins:
+
+      css-loader
+      style-loader
+      extract-text-webpack-plugin
+
+  The css-loader and the style-loader are required to make Webpack understand CSS. By adding these loaders, Webpack will be able to bundle any CSS we require into our bundle.js. The issue with this, is that the JavaScript and the CSS will not load separately on your page, which can lead to UI components not showing up until your JavaScript has loaded. This isn’t great, as on bad networks our design might load very slowly.
+
+  We can get around this issue by adding the extract-text-webpack-plugin.
+*/
+
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 const config = {
   entry: __dirname + "/js/index.jsx",
   output: {
@@ -15,9 +33,19 @@ const config = {
         test: /\.jsx?/,
         exclude: /node_modules/,
         use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader",
+        })      
       }
     ]
   },
+  plugins: [
+    new ExtractTextPlugin("styles.css"),
+  ],
 };
 
 module.exports = config;
